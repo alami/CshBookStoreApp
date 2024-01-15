3a506d16a78f808ca6770c3f82ce876d09420e50
tas_admin <tas_admin@hq.int.unesco.org>
2024-01-15 20:19:27 +0500

    UI.Register [chng API port: 7072->443]

--- BookStoreAppBlazorServerUI/Program.cs
+++ BookStoreAppBlazorServerUI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:7072"));
+builder.Services.AddHttpClient<IClient, Client>(cl => cl.BaseAddress = new Uri("https://localhost:443"));
 
 var app = builder.Build();
 
